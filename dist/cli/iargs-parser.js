80cfe4e5c9d0a30ee71393f4bd0800e187e3182d
Jacob Heater <jacobheater@gmail.com>
2025-06-06 00:12:07 -0400

    Remove deprecated interfaces and classes related to argument parsing and restore point management
    - Deleted IArgsParser and IArgsRule interfaces along with their implementations.
    - Removed KeyValuePair class and StatusCode enum as they are no longer needed.
    - Eliminated RestorePointType enum and associated functions for restore point type validation.
    - Removed exception classes: ArgumentNullException, RestorePointNameInvalidException, and RestorePointTypeInvalidException.
    - Deleted main entry point functions and their related logic for creating restore points.
    - Cleaned up rules for restore point name and type validation, as they are no longer applicable.

--- dist/cli/iargs-parser.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-/**
- * @file Defines the IArgsParser interface.
- * @since 09/01/2018
- * @copyright Jacob Heater <jacobheater@gmail.com>
- */
-Object.defineProperty(exports, '__esModule', { value: true });
-//# sourceMappingURL=iargs-parser.js.map
--- dist/cli/iargs-rule.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-/**
- * @file Defines the IArgsRule interface.
- * @since 09/01/2018
- * @copyright Jacob Heater <jacobheater@gmail.com>
- */
-Object.defineProperty(exports, '__esModule', { value: true });
-//# sourceMappingURL=iargs-rule.js.map
